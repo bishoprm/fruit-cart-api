7a9676e91b72ccc69a8feb747c5f15157bd87aac
bishoprm <bishop.rose@gmail.com>
2022-05-31 20:02:59 +0200

    add cors & use cors options

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,22 @@ import express, { Express, Request, Response } from "express";
 import { router } from "./routes/CartRoutes";
 import "./controllers/CartController";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+const allowedOrigins = ["http://localhost:3001", "https://main--mellifluous-cucurucho-d51433.netlify.app"];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+  credentials: true, //access-control-allow-credentials:true
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(options));
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(router);
 
 app.listen(port, () => {
